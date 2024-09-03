3da9fdda795b2e671fb16da55b474da835613759
adimiuprix <miuprix@gmail.com>
2024-09-03 12:08:23 +0700

    refflink & toastify

--- src/app/friends/page.jsx
+++ src/app/friends/page.jsx
@@ -1,6 +1,32 @@
+'use client'
+import React from 'react'
+import Toastify from 'toastify-js'
 import Image from "next/image"
 
 function Friends(){
+
+    const reffCode = "fdi5786fn"
+    const referralLink = `https://t.me/botname_bot/name_app?startapp=${reffCode}`
+
+    const linkReff = async () => {
+        navigator.clipboard.writeText(referralLink)
+        Toastify({
+            text: `You have copied, your referral link is: ${referralLink}`,
+            duration: 2000,
+            close: true,
+            gravity: "top",
+            position: "left",
+            stopOnFocus: true,
+            style: {
+                background: 'linear-gradient(to right, #ff6f61, #de6161)',
+                color: '#fff',
+                borderRadius: '12px',
+                padding: '12px 24px',
+                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+            },
+        }).showToast()
+    }
+
     return(
         <div class="flex min-h-[calc(100vh-100px)] flex-col px-6">
             <p class="mt-6 text-center text-[30px] font-semibold leading-[1.15]">Invite frens <br />and get more CATS</p>
@@ -11,7 +37,7 @@ function Friends(){
             <div class="grid gap-3">
                 <p class="font-medium leading-snug text-center opacity-50">Tap on the button to invite your friends</p>
 
-                <button class="px-4 py-3 w-full font-bold text-white bg-gradient-to-r from-blue-400 to-blue-600 rounded-lg shadow-lg transition duration-300 hover:from-blue-500 hover:to-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                <button onClick={linkReff} class="px-4 py-3 w-full font-bold text-white bg-gradient-to-r from-blue-400 to-blue-600 rounded-lg shadow-lg transition duration-300 hover:from-blue-500 hover:to-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
                     Invite friends
                 </button>
 
