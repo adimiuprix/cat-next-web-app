85043ffb1ff04cabf75491433650594f683eefa7
adimiuprix <miuprix@gmail.com>
2024-09-04 10:23:24 +0700

    page home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,10 +16,10 @@ export default function Home() {
         <div className="flex relative flex-col">
             <div className="grow pb-[70px]">
                 <div className="px-6 py-3">
-                    <span class="flex gap-2 items-center px-3 py-1 mx-auto w-max text-sm font-semibold bg-gradient-to-r from-sky-100 to-purple-100 rounded-lg">
-                        <span class="flex gap-1 items-center">
-                            <img src="/nomis-logo.svg" alt="nomis" />
-                            <span class="text-lg font-medium">+</span>
+                    <span className="flex gap-2 items-center px-3 py-1 mx-auto w-max text-sm font-semibold bg-gradient-to-r from-sky-100 to-purple-100 rounded-lg">
+                        <span className="flex gap-1 items-center">
+                            <Image src="/nomis-logo.svg" height={100} width={100} alt="nomis" />
+                            <span className="text-lg font-medium">+</span>
                             <span>🐈‍⬛</span>
                         </span>
                         GET MORE CATS WITH NOMIS NOW
@@ -31,13 +31,13 @@ export default function Home() {
                         <span className="inline-block font-semibold">818 CATS {idUser}</span>
                     </div>
 
-                    <div class="flex gap-2 justify-evenly items-center mb-8">
+                    <div className="flex gap-2 justify-evenly items-center mb-8">
                         <Income title={"Reward"} score={0} />
                         <Income title={"Task"} score={0} />
                         <Income title={"Invites"} score={0} />
                     </div>
 
-                    <p class="mb-5 text-2xl font-semibold">Tasks</p>
+                    <p className="mb-5 text-2xl font-semibold">Tasks</p>
 
                     <div class="grid gap-3 mb-4">
                         <Task nameTask={"Follow youtube"} reward={"10"} />
@@ -47,19 +47,19 @@ export default function Home() {
                         <Task nameTask={"Tonton youtube"} reward={"100"} />
                     </div>
 
-                    <p class="mb-4 text-2xl font-semibold">
+                    <p className="mb-4 text-2xl font-semibold">
                         Cats <span class="text-xl">&</span> Frens
                     </p>
 
-                    <div class="grid gap-2 pb-4">
-                        <div class="flex gap-10 justify-between items-center">
-                            <div class="flex gap-2 items-center">
-                                <span class="grid place-content-center bg-gradient-to-tr from-teal-50 to-sky-100 rounded-full size-10">
-                                    <img src="/bitget-logo.png" alt="bitget logo" class="size-8" />
+                    <div className="grid gap-2 pb-4">
+                        <div className="flex gap-10 justify-between items-center">
+                            <div className="flex gap-2 items-center">
+                                <span className="grid place-content-center bg-gradient-to-tr from-teal-50 to-sky-100 rounded-full size-10">
+                                    <Image src="/bitget-logo.png" width={100} height={100} alt="bitget logo" class="size-8" />
                                 </span>
-                                <span class="font-medium">Bitget Wallet</span>
+                                <span className="font-medium">Bitget Wallet</span>
                             </div>
-                            <button class="inline-flex items-center justify-center whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 text-sm gap-1.5">
+                            <button className="inline-flex items-center justify-center whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 text-sm gap-1.5">
                                 <span>Complete</span>
                             </button>
                         </div>
